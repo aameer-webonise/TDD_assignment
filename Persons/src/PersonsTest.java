3a4ae649d3171fe0ef44730a65c9f66a2d597bbd
aameer-webonise <aameer.ausekar@weboniselab.com>
2016-03-07 09:47:39 +0530

    modify 8th assignment

--- Persons/src/PersonsTest.java
+++ Persons/src/PersonsTest.java
@@ -7,22 +7,18 @@ import org.junit.Test;
 
 
 public class PersonsTest {
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
+	
 
 	@Before
 	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
 	}
 
 	@Test
 	public void test() {
 		assertEquals("1Aameer21Male", new Persons(1,"Aameer",21,"Male").toString());
 		assertEquals("2Viresh25Male", new Persons(2,"Viresh",25,"Male").toString());
+		assertEquals("3 25Female", new Persons(3," ",25,"Female").toString());
+		assertEquals("4,arjun20}{Male", new Persons(4,",arjun",20,"}{Male").toString());
 	}
 
 }
