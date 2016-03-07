97c578944311406b73217cefe4c8ae6c60c29ff0
aameer-webonise <aameer.ausekar@weboniselab.com>
2016-03-07 09:45:32 +0530

    modify 2nd assignment

--- EmailValidation/src/EmailValidation.java
+++ EmailValidation/src/EmailValidation.java
@@ -2,9 +2,7 @@
 public class EmailValidation {
 boolean validate(String email)
 	{
-	//String EMAIL_REGEX = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
 	String EMAIL_REGEX= "^[a-zA-Z]+[A-Za-z0-9+_.-]+@(.+)$";
-	//String EMAIL_REGEX= "^[a-zA-Z]+[*_.-]+[A-Za-z0-9]+@(.+)$";
 		return email.matches(EMAIL_REGEX);
 	}
 }
